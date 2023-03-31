728cabddb8191216a2b2af2da251dd7613b089f1
eugene-dim <dimovjenea@gmail.com>
2023-03-31 16:42:56 -0700

    reupload due to error

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,36 +1,45 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const DB_NAME = 'jate';
+const OBJ_STORE = 'jate';
+const DB_VERSION = 1;
+
+async function initDB() {
+  const db = await openDB(DB_NAME, DB_VERSION, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains(OBJ_STORE)) {
+        console.log(`${OBJ_STORE} object store already exists`);
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore(OBJ_STORE, { keyPath: 'id', autoIncrement: true });
+      console.log(`${OBJ_STORE} object store created`);
     },
   });
+  return db;
+}
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const getAllFromDb = async () => {
-  const dbName = 'jateStore';
-  const jateStoreDb = await openDB(dbName, 1);
-  const transaction = jateStoreDb.transaction(dbName, 'readonly');
-  const objectStore = transaction.objectStore(dbName);
-  const getAllRequest = objectStore.getAll();
-  const allData = await getAllRequest;
-  return allData.length === 0 ? null : allData;
-  };
+export async function putDb(content) {
+  console.log('Putting data into the database...');
+  const db = await initDB();
+  const tx = db.transaction(OBJ_STORE, 'readwrite');
+  const store = tx.objectStore(OBJ_STORE);
+  const result = await store.add({ jate: content });
+  console.log('Data saved with key', result);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const addToDb = async (content) => {
-  const dbName = 'jateStore';
-  const jateStoreDb = await openDB(dbName, 1);
-  const transaction = jateStoreDb.transaction(dbName, 'readwrite');
-  const objectStore = transaction.objectStore(dbName);
-  const putRequest = objectStore.put({ content, id: 1 });
-  const requestResult = await putRequest;
-  };
-  
-initdb();
+export async function getDb() {
+  console.log('Getting all data from the database...');
+  const db = await initDB();
+  const tx = db.transaction(OBJ_STORE, 'readonly');
+  const store = tx.objectStore(OBJ_STORE);
+  const result = await store.getAll();
+  if (result.length === 0) {
+    console.log('No data found in the database');
+    return null;
+  } else {
+    console.log('Data retrieved', result);
+    return result;
+  }
+}
+
+initDB();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,28 +1,39 @@
-const butInstall = document.getElementById('buttonInstall');
+const buttonInstall = document.getElementById('buttonInstall');
+let deferredPrompt = null;
+
+function handleBeforeInstallPrompt(event) {
+  // Store the triggered events
+  deferredPrompt = event;
+
+  // Remove the hidden class from the button.
+  buttonInstall.classList.remove('hidden');
+}
+
+async function handleInstallClick() {
+  if (!deferredPrompt) {
+    return;
+  }
+
+  // Show prompt
+  deferredPrompt.prompt();
+
+  // Reset the deferred prompt variable, it can only be used once.
+  deferredPrompt = null;
+
+  buttonInstall.classList.add('hidden');
+}
+
+function handleAppInstalled(event) {
+  // Clear prompt
+  deferredPrompt = null;
+}
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {event.preventDefault();
-    deferredPromptEvent = event;
-    installButton.classList.remove('hidden');});
- 
+window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {if (!deferredPromptEvent) return;
-    deferredPromptEvent.prompt();
-    
-    const choiceResult = await deferredPromptEvent.userChoice;
-    deferredPromptEvent = null;
-    installButton.classList.add('hidden');
-    
-    console.log(`User ${choiceResult.outcome} the installation`);
-    });
+buttonInstall.addEventListener('click', handleInstallClick);
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {deferredPromptEvent = null;
-
-    // Hide the install button
-    installButton.classList.add('hidden');
-  
-    // Log the installation event
-    console.log('App installed successfully');
-  });
+window.addEventListener('appinstalled', handleAppInstalled);
